fac3c4a1c2a9507de07bc47ce530625e2dd5adfa
1e10computer <sample@bths.edu>
2023-12-18 10:22:33 -0500

    testing

--- src/TreasureHunterRunner.java
+++ src/TreasureHunterRunner.java
@@ -2,5 +2,6 @@ public class TreasureHunterRunner {
     public static void main(String[] args) {
         TreasureHunter game = new TreasureHunter();
         game.play();
+        System.out.println("Testing - Shubhechchha");
     }
 }
